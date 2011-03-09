fd31990108e0bf2c53cf4048ada97068e95c70de
jaqinetik <devnull@localhost>
2011-03-08 20:15:14 -0500

    Began working on the Kinetic API and changed the name of KinectTest to KineticTest.

---
+++ KineticControl/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace KineticControl
+{
+    abstract class Model
+    {
+        private String fixtureAddress;
+        private Colors currentColor;
+
+
+/*
+ * Getters and Setter for the current  color 
+ */
+
+        public Colors getCurrentColor()
+        {
+            return currentColor;
+        }
+
+        public void setCurrentColor(Colors currentColor)
+        {
+            this.currentColor = currentColor;
+        }
+/*
+ * Getters and setters for the current fixture address
+ */
+
+        public String getFixtureAddress()
+        {
+            return fixtureAddress;
+        }
+
+        public void setFixtureAddress(String fixtureAddress)
+        {
+            this.fixtureAddress = fixtureAddress;
+        }
+
+    }
+}
