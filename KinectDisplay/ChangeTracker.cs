c3bcd6d7e6af7124c9f7162f39d2ed9f071c9bde
junruh <devnull@localhost>
2011-04-10 15:03:23 -0400

    *See previous checkin

---
+++ KinectDisplay/ChangeTracker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Media;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using KineticControl;
+
+namespace KinectDisplay
+{
+    class ChangeTracker
+    {
+        private readonly ColorData _colorState;
+        //public int[] _lastUpdated;
+
+        private readonly Image<Gray, byte> _lastImage;
+
+        private readonly DateTime _lastUpdate;
+
+        private readonly Image<Gray, byte> _diffImg;
+
+        private static readonly Gray Black = new Gray(0.0);
+        private static readonly Gray White = new Gray(256.0);
+
+        public ChangeTracker(ColorData colorData)
+        {
+            _colorState = colorData;
+            for (int i = 0; i < colorData.Count; i++ )
+            {
+                colorData[i] = Colors.Black;
+            }
+            //_lastUpdated = new int[colorData.Count];
+            _lastUpdate = DateTime.Now;
+            _lastImage = null;
+            _diffImg = new Image<Gray, byte>(640,480);
+        }
+
+        public Image<Gray, byte> UpdateImage(Image<Gray, byte> image)
+        {
+            UpdateColors();
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    Gray baseGray = _lastImage[i, j];
+                    Gray topGray = image[i, j];
+                    double topIntensity = topGray.Intensity;
+                    if (topGray.Intensity == 0.0 || baseGray.Intensity == 0)
+                    {
+                        _diffImg[i, j] = Black;
+                    }
+                    else
+                    {
+                        double baseIntensity = baseGray.Intensity;
+                        if (ForegroundDetector.CompareDoubles(baseIntensity, topIntensity))
+                        {
+                            _diffImg[i, j] = Black;
+                        }
+                        else
+                        {
+                            _diffImg[i, j] = White;
+                        }
+                    }
+                }
+            }
+            _diffImg.Erode(3);
+
+            for (int i = 0; i < _diffImg.Height; i++ )
+            {
+                for(int j =0; j < _diffImg.Width; j++)
+                {
+                    if(_diffImg[i,j].Intensity == 256.0)
+                    {
+                        UpdateBucket(image[i, j].Intensity);
+                    }
+                }
+            }
+
+            return _diffImg;
+        }
+
+        private void UpdateColors()
+        {
+            TimeSpan delta = DateTime.Now - _lastUpdate;
+            double adjfactor = Math.Pow(2, delta.Seconds);
+
+            for (int i = 0; i < _colorState.Count; i++)
+            {
+                Color color = _colorState[i];
+                color.R = (byte) (color.R/adjfactor);
+                color.G = (byte) (color.G/adjfactor);
+                color.B = (byte) (color.B/adjfactor);
+                _colorState[i] = color;
+            }
+        }
+
+        private void UpdateBucket(double intensity)
+        {
+            int pos = (int)Math.Floor(intensity/250.0*49);
+            _colorState[pos] = Colors.Red;
+        }
+
+        public ColorData ColorData { get { return _colorState; } }
+    }
+}
---
+++ KineticControl/ColorData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace KineticControl
+{
+    public class ColorData : IEnumerable<Color>
+    {
+        private readonly byte[] _byteArray;
+        private readonly int _initialLength;
+
+        public ColorData(byte[] initialData, int length)
+        {
+            _initialLength = initialData.Length;
+            _byteArray = new byte[_initialLength + length*3];
+            _byteArray.Initialize();
+            initialData.CopyTo(_byteArray, 0);
+        }
+
+        public int Count { get { return (_byteArray.Length - _initialLength)/3; } }
+
+        public byte[] Bytes { get { return _byteArray; } }
+
+        public Color this[int pos]
+        {
+            get
+            {
+                if(_initialLength + pos*3 >= _byteArray.Length) throw new IndexOutOfRangeException();
+                Color color = new Color
+                                  {
+                                      R = _byteArray[_initialLength + pos*3],
+                                      G = _byteArray[_initialLength + pos*3 + 1],
+                                      B = _byteArray[_initialLength + pos*3 + 2]
+                                  };
+                return color;
+            }
+            set
+            {
+                if (_initialLength + pos * 3 > _byteArray.Length) throw new IndexOutOfRangeException();
+                _byteArray[_initialLength + pos*3] = value.R;
+                _byteArray[_initialLength + pos*3 + 1] = value.G;
+                _byteArray[_initialLength + pos*3 + 2] = value.B;
+            }
+        }
+
+        public IEnumerator<Color> GetEnumerator()
+        {
+           int length = Count;
+           for(int i=0; i<length; i++)
+           {
+               yield return this[i];
+           }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
