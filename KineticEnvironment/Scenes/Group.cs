9c8451b297d114dafe5da5c97d7593e173e90ca4
joseph <devnull@localhost>
2013-06-23 20:23:34 -0700

    Added Simulation system

---
+++ KineticEnvironment/Scenes/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KineticControl;
+
+namespace RevKitt.KS.KineticEnvironment.Scenes
+{
+    class Group
+    {
+        private readonly string _name;
+        private readonly IList<LightAddress> _lights; 
+
+
+        public Group(string name, IEnumerable<LightAddress> lights )
+        {
+            _name = name;
+            _lights = new List<LightAddress>(lights).AsReadOnly();
+        }
+
+        public string Name { get { return _name; } }
+
+        public IList<LightAddress> Lights { get { return _lights; } } 
+    }
+}
