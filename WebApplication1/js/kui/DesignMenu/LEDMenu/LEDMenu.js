60ffe505bf333ac3f2efc339212016fa5fe99620
jacqui <devnull@localhost>
2013-10-21 11:38:39 -0700

    *Css changes to design menu. *led menu refactorings

--- WebApplication1/js/kui/DesignMenu/LEDMenu/LEDMenu.js
+++ WebApplication1/js/kui/DesignMenu/LEDMenu/LEDMenu.js
@@ -2,124 +2,33 @@
 define([
     "dojo/_base/declare",
     "kui/util/CommonHTML",
-    "dojo/dom",
-    "dijit/layout/ContentPane",
-    "dojo/dom-style",
     "dojo/dom-construct",
-    "threejs/three",
     "kui/util/CommonFormItems",
-    "dijit/TitlePane",
     "dojo/on",
     "dojo/_base/array",
     "kui/ajax/Scenes",
     "kui/ModelView/Node/LightAddress",
     "kui/DesignMenu/AccordianItem",
     "dojo/dom-class"],
-    function (declare, html, dom, ContentPane, domStyle, domConstruct, three, CommonForm,
-        TitlePane, on, array, Scenes, LightAddress, AccordianItem, domClass) {
+    function (declare, html,  domConstruct, CommonForm,  on, array, Scenes, LightAddress, AccordianItem, domClass) {
         "use strict";
         return declare("kui.DesignMenu.LEDMenu.LEDMenu", AccordianItem, {
                    
-            /*
-             *   Left menu for Kinect 3D model design 
-             *
-             */
 
             constructor: function () {
 
-               // this.style = "background-color:#141414;";           
                 this.title = "LED Menu";
-
-                /*CSS Stylings*/
-                /*
-                this.tableCellBorderColor = "#333333";
-
-                this.mainBackgroundColor = "background:linear-gradient(27deg, #151515 5px, transparent 5px) 0 5px," +
-                         "linear-gradient(207deg, #151515 5px, transparent 5px) 10px 0px," +
-                         "linear-gradient(27deg, #222 5px, transparent 5px) 0px 10px," +
-                         "linear-gradient(207deg, #222 5px, transparent 5px) 10px 5px," +
-                         "linear-gradient(90deg, #1b1b1b 10px, transparent 10px)," +
-                         "linear-gradient(#1d1d1d 25%, #1a1a1a 25%, #1a1a1a 50%, transparent 50%, transparent 75%, #242424 75%, #242424);" +
-               "background-color: #131313;" +
-               "background-size: 20px 20px;";*/
-
-
             },
 
             createLEDMenu: function (container) {
-              /*  var contentPane = new ContentPane(
-                  {
-                      title: "LED Menu",
-                      style: this.mainBackgroundColor,
-                      onShow: dojo.hitch(container.simulation, container.simulation.setSceneMode)
-
-                  });*/
-
-                //var spacerDivStyle = "width:100%;height:7px;";
-
-                //container.addChild(contentPane);
-
-               // var titlePaneDiv = html.createDiv("width:100%;height:100%;");
-                //domConstruct.place(titlePaneDiv, contentPane.domNode);
-
-                //var nodeDiv = this.createNodeSection();
-                              
-                /*Node Creation Section*/
-               /* var nodePane = new TitlePane({
-
-                    title: "Create / Select Nodes",
-                    content: nodeDiv
-
-                });
-
-                nodeDiv.parentNode.setAttribute('style', 'background-color:#141414;');
-                domConstruct.place(nodePane.domNode, titlePaneDiv);
-                domConstruct.place(html.createDiv(spacerDivStyle), titlePaneDiv);
-
-                          
-                /*Node Removal Section*/
-                /*var removalDiv = this.createRemoveNodeSection();
-
-                var removePane = new TitlePane({
-
-                    title: "Remove Nodes",
-                    content: removalDiv
-
-                });
-
-                removalDiv.parentNode.setAttribute('style', this.mainBackgroundColor);
-                domConstruct.place(removePane.domNode, titlePaneDiv);
-                domConstruct.place(html.createDiv(spacerDivStyle), titlePaneDiv);
-
-                /*Group Section*/            
-               /* var groupDiv = this.createGroupSection();
-
-                var groupPane = new TitlePane({
-
-                    title: "Group Nodes",
-                    content: groupDiv
-
-                });
-
-                groupDiv.parentNode.setAttribute('style', this.mainBackgroundColor);
-                domConstruct.place(groupPane.domNode, titlePaneDiv);
-                domConstruct.place(html.createDiv(spacerDivStyle), titlePaneDiv);*/
-
-                //contentPane.startup();
-
-                this.onShow = function () {
-
-                    //this.patternModel.updateGroupDropDown();
-                   // container.simulation.setPatternMode();
-                };
-
-                domConstruct.place(this.domNode, container.domNode);
-
+            
                 this._createNodes();
                 this._createSelectNodes();
                 this._createGroupBox();
 
+                this.onShow = dojo.hitch(container.simulation, container.simulation.setSceneMode);
 
+                domConstruct.place(this.domNode, container.domNode);
                 domClass.add(this.domNode, "designMenu");
             },
 
@@ -281,7 +190,7 @@ define([
                 var thisObj = this;
                 var selectVertexButton = CommonForm.createButton('Select All Vertices', function () {
 
-                  //  thisObj.sceneModel.sceneInteraction.ledSet.selectAllVertexs();
+                  thisObj.sceneModel.sceneInteraction.ledSet.selectAllVertexs();
                 });
 
                 domConstruct.place(selectVertexButton.domNode, div);
@@ -299,7 +208,7 @@ define([
                 var thisObj = this;
                 var deselectVertexButton = CommonForm.createButton('Deselect All Vertices', function () {
 
-                    //  thisObj.sceneModel.sceneInteraction.ledSet.selectAllVertexs();
+                    thisObj.sceneModel.sceneInteraction.ledSet.selectAllVertexs();
                 });
 
                 domConstruct.place(deselectVertexButton.domNode, div);
@@ -325,13 +234,15 @@ define([
                 return {
                     valueContent: div
                 }
-
             },
 
             _createGroupBox: function(){
 
                 var groupDivs = this.createTitlePane("Create Groups");
 
+                var groupNameTableItems = [];
+                groupNameTableItems.push(this._createGroupNameBox());
+                this.addTableItem(groupNameTableItems, groupDivs.contentDiv); 
                 this.addDivItem(this._createGroupListBoxSection(), groupDivs.contentDiv);
                 var addRemoveTableItems = [];
                 addRemoveTableItems.push(this._createAddRemoveGroupSection());
@@ -344,7 +255,7 @@ define([
 
                 var div = html.createDiv();
                 var groupListBox = CommonForm.createListBox("width:89%;");
-                //this.sceneModel.sceneInteraction.groupModel.ledGroupListBox = groupListBox;
+                this.sceneModel.sceneInteraction.groupModel.ledGroupListBox = groupListBox;
 
                 domConstruct.place(groupListBox.domNode, div);
 
@@ -353,20 +264,32 @@ define([
                 }
             },
 
+            _createGroupNameBox: function () {
+
+                var groupNametextBox = CommonForm.createTextBox("", "Group Name", "text-align:left;width:100%;");
+                //this.patternModel.setNameField(nameField); //refactor this out and use update handlers instea
+
+                return {
+                    title: "Group Name",
+                    valueContent: groupNametextBox.domNode
+                };
+
+            },
+
             _createAddRemoveGroupSection: function(){
 
 
                 var thisObj = this;
                 var addButton = CommonForm.createButton('Add Group', function () {
                     var group = thisObj.sceneModel.sceneInteraction.groupModel.createGroupFromSelected(groupNameTextBox.get('value'));
-                    thisObj.addGroup(group);
+                    thisObj._addGroup(group);
                 }, null);
 
                 CommonForm.setButtonStyle(addButton, 90);
 
                 var removeButton = CommonForm.createButton('Remove Group', function () {
 
-                    thisObj.removeSelected();
+                    thisObj._removeSelected();
 
                 }, null);
 
@@ -377,100 +300,21 @@ define([
                     removeButton: removeButton.domNode
                 }
             },
-
-
-            createGroupSection: function () {
-
-                var div = html.createDiv();
-                var groupListBox = CommonForm.createListBox("width:89%;");
-                this.sceneModel.sceneInteraction.groupModel.ledGroupListBox = groupListBox;
-
-                var groupNameTable = html.createTable("margin-left:auto;" +
-                    "margin-right:auto;" +
-                    "border-spacing: 0px;"+
-                    "width:95%;");
-                domConstruct.place(groupNameTable, div);
-
-                var groupNameRow = html.createRow("background-color:#141414;");
-                domConstruct.place(groupNameRow, groupNameTable);
-
-                var groupNameTitleCell = html.createCell("border-bottom-left-radius: 7px;" +
-                    "border-top-left-radius: 7px;" +
-                    "border-left: 2px solid" + this.tableCellBorderColor+";"+
-                    "border-top: 2px solid" + this.tableCellBorderColor + ";" +
-                    "border-bottom: 2px solid" + this.tableCellBorderColor + ";" +
-                    "color:#3d8dd5;" +
-                    "width:40%;"+
-                    "text-align: center");
-
-                domConstruct.place(groupNameTitleCell, groupNameRow);
-                groupNameTitleCell.innerHTML = "Group Name";
-
-                var groupNameValueCell = html.createCell("text-align:center;" +
-                    "border-top: 2px solid #2d2d2d;" +
-                    "border-right: 2px solid #2d2d2d;" +                  
-                    "border-bottom: 2px solid" + this.tableCellBorderColor + ";" +
-                    "border-left-radius: 7px;" +
-                    "border-right-radius: 7px;" +
-                    "width: 60%");
-
-                domConstruct.place(groupNameValueCell, groupNameRow);
-
-                var groupNameTextBox = CommonForm.createTextBox("", "Group Name", "width:100%;");
-                domConstruct.place(groupNameTextBox.domNode, groupNameValueCell);
-
-                this.groupListBox = groupListBox;
-                
-                var obj = this;
-
-                var addButton = CommonForm.createButton('Add Group', function () {
-                    var group = obj.sceneModel.sceneInteraction.groupModel.createGroupFromSelected(groupNameTextBox.get('value'));
-                    obj.addGroup(group);
-                }, null, "color:#3d8dd5;");
-                domStyle.set(addButton.domNode.firstChild, "width", "100px");
-             
-                var removeButton = CommonForm.createButton('Remove Group', function () {
-
-                    obj.removeSelected();
-
-                }, null, "color:#3d8dd5;");
-
-                domStyle.set(removeButton.domNode.firstChild, "width", "100px");
-
-                var listDiv = html.createDiv("text-align:center;" +
-                    "margin-right:auto;" +
-                    "margin-left:auto;" +
-                    "background-color:#141414;"+
-                    "border: 2px solid #282828;"+
-                    "border-radius:7px;");
-
-                domConstruct.place(groupListBox.domNode, listDiv);
-                domConstruct.place(listDiv, div);
-
-                var buttonDiv = html.createDiv("text-align:center;");
-                domConstruct.place(addButton.domNode, listDiv);
-                domConstruct.place(removeButton.domNode, listDiv);                
-                domConstruct.place(buttonDiv, div);
-
-                this.setGroupNames();
-
-                return div;
-            },
             
-            setGroupNames: function () {
-                var obj = this;
+            _setGroupNames: function () {
+
+                var thisObj = this;
 
                 dojo.empty(this.groupListBox.domNode.children);
                 
                 Scenes.getGroupNames(function (groupNames) {
                     array.forEach(groupNames, function (groupName) {
-                        obj.addGroup(groupName);
-                       
+                        thisObj._addGroup(groupName);                       
                     });
                 });
             },
 
-            addGroup: function(groupName) {
+            _addGroup: function(groupName) {
                 var option = html.createOption(groupName);
                 var thisObj = this;
                 this.groupListBox.domNode.appendChild(option);
@@ -484,7 +328,7 @@ define([
                 });
             },
             
-            removeSelected: function () {
+            _removeSelected: function () {
                 var thisObj = this;
                 array.forEach(this.groupListBox.getSelected(), function (node) {
                     thisObj.sceneModel.sceneInteraction.groupModel.removeGroup(node.innerHTML);
